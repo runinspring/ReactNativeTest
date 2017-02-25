ea340b4412a7d545bd969c0aee5d4c39c193939a
张宇 <runinspring@gmail.com>
2017-02-25 18:52:23 +0800

    update

--- ios/index.ios.js
+++ ios/index.ios.js
@@ -3,22 +3,49 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-
+var Dimensions = require('Dimensions');
+var {width,height} = Dimensions.get('window');
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
-  Text,
+  Text,TextInput,
+  Button,
   View
 } from 'react-native';
-
+var TEXT_REF = 'text1_reg';
 export default class ios extends Component {
+  state={
+    sysinfo:'',
+    input:''
+  }
+  componentDidMount(){
+    // console.log('componentDidMount:')
+  }
+  onGetSysinfo(){
+    // var out = document
+    // var out = this.refs.welcome.width;
+    // console.log(this.refs.welcome.style)
+    // console.log(this.refs[TEXT_REF].style)
+    // this.refs.welcome.innerHTML = "sdd"
+    // this.refs.welcome.setNativeProps({
+    //   style:{
+    //     color:'blue',
+    //     fontSize:30
+    //   }
+    // });
+    this.setState({sysinfo:`宽:${width},高:${height}`})
+  }
   render() {
+    console.log('render',width,height)
+
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
+        <Text ref='welcome' style={styles.welcome}>
+          Welcome to React Native2!
         </Text>
+        <TextInput ref="ipp" placeholder={"dsdfdf"}/>
+
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
         </Text>
@@ -26,6 +53,12 @@ export default class ios extends Component {
           Press Cmd+R to reload,{'\n'}
           Cmd+D or shake for dev menu
         </Text>
+        <Button title='获取系统信息'onPress={this.onGetSysinfo.bind(this)}/>
+        <Text style={styles.instructions}>
+          {this.state.sysinfo}
+        </Text>
+        <Text style={{width:100,height:100,backgroundColor:"#000000"}}/>
+
       </View>
     );
   }
