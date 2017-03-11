f8bc946ef039cdabda4c1b15fd56fab09c868995
张宇 <runinspring@gmail.com>
2017-03-11 23:06:58 +0800

    upgrade

--- ios/src/pages/Buttons.js
+++ ios/src/pages/Buttons.js
@@ -1,21 +1,24 @@
-
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
     StyleSheet,
-    View, Text,TouchableHighlight
+    View, Text, TouchableHighlight
 } from 'react-native';
 export default class Buttons extends Component {
     onButtonPress(id) {
-        console.log('onButtonPress:',id)
+        console.log('onButtonPress:', id)
     }
+
     render() {
         var {styles} = this.props;
         console.log('Sysinfo2.render')
         return (
             <View style={[styles.container, styles.xCenter, stylesC.container]}>
-                <TouchableHighlight style={stylesC.buttons} onPress={this.onButtonPress.bind(this, 0)}>
-                    <Text style={stylesC.button1}>按钮样式 style-1</Text>
-                </TouchableHighlight>
+                <View style={stylesC.buttons}>
+                    <TouchableHighlight style={[stylesC.button1]}
+                                        onPress={this.onButtonPress.bind(this, 0)}>
+                        <Text style={stylesC.button1Text}>按钮样式 style-1</Text>
+                    </TouchableHighlight>
+                </View>
                 <TouchableHighlight style={stylesC.buttons} onPress={this.onButtonPress.bind(this, 0)}>
                     <Text style={stylesC.button2}>按钮样式 style-2</Text>
                 </TouchableHighlight>
@@ -30,7 +33,15 @@ const stylesC = StyleSheet.create({
     buttons: {
         paddingBottom: 20,
     },
-    button1: {
+    button1:{
+        backgroundColor:'rgba(0,0,0,0.4)',
+        borderLeftWidth:3,
+        borderLeftColor:'#000000',
+        borderStyle:'solid',
+        borderRadius:4,
+        padding: 10,
+    },
+    button1Text: {
         color: 'red',
         fontSize: 12,
     },
